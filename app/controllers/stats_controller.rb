f4b18ed68f7e95f4802ddedf7f3be561f14463d3
Abel Normand <abel.ze.normand@gmail.com>
2017-03-04 00:30:47 +0500

    fix

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,8 +1,11 @@
 class StatsController < ApplicationController
   def load_stats
     Vk::StartStatsJobService.new(
-
-    )
-
+      group_id: params[:group_id],
+      access_token: session[:access_token],
+      # TODO remove this, hardcode
+      date_from: "1970-01-01",
+      date_to: "2017-01-01"
+    ).call
   end
 end
