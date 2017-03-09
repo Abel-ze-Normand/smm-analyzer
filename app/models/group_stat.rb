56846855d8f124d660aed8fadf55ee4db3e87c09
Abel Normand <abel.ze.normand@gmail.com>
2017-03-09 14:08:56 +0500

    fixed assotiations

--- app/models/group_stat.rb
+++ app/models/group_stat.rb
@@ -1,5 +1,5 @@
 class GroupStat < ApplicationRecord
-  has_one :age_cluster
+  has_one :age_cluster, dependent: :destroy
   belongs_to :group
-  has_many :posts
+  has_many :posts, dependent: :destroy
 end
